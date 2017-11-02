dfcb613b49acbc8106fb155d3e7da5c0645384d5
Rob Fallows <rfallows@int.centiq.co.uk>
2017-11-02 16:23:50 +0000

    Add try/catch around aggregation

--- aggregate.js
+++ aggregate.js
@@ -14,23 +14,27 @@ export const ReactiveAggregate = (sub, collection, pipeline, options) => {
   const update = async () => {
     if (initializing) return;
     // add and update documents on the client
-    const docs = await collection.rawCollection().aggregate(pipeline, options.aggregationOptions);
-    docs.forEach(doc => {
-      if (!sub._ids[doc._id]) {
-        sub.added(options.clientCollection, doc._id, doc);
-      } else {
-        sub.changed(options.clientCollection, doc._id, doc);
-      }
-      sub._ids[doc._id] = sub._iteration;
-    });
-    // remove documents not in the result anymore
-    Object.keys(sub._ids).forEach((v, k) => {
-      if (v !== sub._iteration) {
-        delete sub._ids[k];
-        sub.removed(options.clientCollection, k);
-      }
-    });
-    sub._iteration++;
+    try {
+      const docs = await collection.rawCollection().aggregate(pipeline, options.aggregationOptions);
+      docs.forEach(doc => {
+        if (!sub._ids[doc._id]) {
+          sub.added(options.clientCollection, doc._id, doc);
+        } else {
+          sub.changed(options.clientCollection, doc._id, doc);
+        }
+        sub._ids[doc._id] = sub._iteration;
+      });
+      // remove documents not in the result anymore
+      Object.keys(sub._ids).forEach((v, k) => {
+        if (v !== sub._iteration) {
+          delete sub._ids[k];
+          sub.removed(options.clientCollection, k);
+        }
+      });
+      sub._iteration++;
+    } catch (err) {
+      throw err;
+    }
   }
 
   // track any changes on the collection used for the aggregation
