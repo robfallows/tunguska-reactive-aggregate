294e2c1913b78934610dfdce3e88e71a6551b5a0
Rob Fallows <rob.fallows@gmail.com>
2019-03-26 16:47:10 +0000

    Initial beta code

--- aggregate.js
+++ aggregate.js
@@ -1,18 +1,26 @@
 export const ReactiveAggregate = (sub, collection, pipeline, options) => {
-  import {
-    Promise
-  } from 'meteor/promise';
-  const defaultOptions = {
-    aggregationOptions: {},
-    observeSelector: {},
-    observeOptions: {},
-    observers: [], // cursor1, ... cursorn
-    debounceDelay: 100, // mS
-    debounceCount: 100,
-    clientCollection: collection._name
-  };
-  defaultOptions = { ...defaultOptions, ...options };
+  import { Meteor } from 'meteor/meteor';
+  import { Promise } from 'meteor/promise';
 
+  const localOptions = {
+    ...{
+      autoObserver: true,
+      aggregationOptions: {},
+      observeSelector: {},
+      observeOptions: {},
+      observers: [], // cursor1, ... cursorn
+      debounceDelay: 100, // mS
+      debounceCount: 100,
+      clientCollection: collection._name
+    },
+    ...options
+  };
+
+  if (Object.keys(localOptions.observeSelector).length != 0) console.log('tunguska:reactive-aggregate observeSelector is deprecated');
+  if (Object.keys(localOptions.observeOptions).length != 0) console.log('tunguska:reactive-aggregate observeOptions is deprecated');
+
+  // observeChanges() will immediately fire an "added" event for each document in the query
+  // these are skipped using the initializing flag
   let initializing = true;
   sub._ids = {};
   sub._iteration = 1;
@@ -21,12 +29,12 @@ export const ReactiveAggregate = (sub, collection, pipeline, options) => {
     if (initializing) return;
     // add and update documents on the client
     try {
-      const docs = await collection.rawCollection().aggregate(pipeline, options.aggregationOptions).toArray();
+      const docs = await collection.rawCollection().aggregate(pipeline, localOptions.aggregationOptions).toArray();
       docs.forEach(doc => {
         if (!sub._ids[doc._id]) {
-          sub.added(options.clientCollection, doc._id, doc);
+          sub.added(localOptions.clientCollection, doc._id, doc);
         } else {
-          sub.changed(options.clientCollection, doc._id, doc);
+          sub.changed(localOptions.clientCollection, doc._id, doc);
         }
         sub._ids[doc._id] = sub._iteration;
       });
@@ -35,7 +43,7 @@ export const ReactiveAggregate = (sub, collection, pipeline, options) => {
       Object.keys(sub._ids).forEach(id => {
         if (sub._ids[id] !== sub._iteration) {
           delete sub._ids[id];
-          sub.removed(options.clientCollection, id);
+          sub.removed(localOptions.clientCollection, id);
         }
       });
       sub._iteration++;
@@ -49,7 +57,7 @@ export const ReactiveAggregate = (sub, collection, pipeline, options) => {
 
   const debounce = () => {
     if (initializing) return;
-    if (!timer) timer = Meteor.setTimeout(update, options.debounceDelay);
+    if (!timer && localOptions.debounceCount > 0) timer = Meteor.setTimeout(update, localOptions.debounceDelay);
     if (currentDebounceCount++ > localOptions.debounceCount) {
       currentDebounceCount = 0;
       Meteor.clearTimeout(timer);
@@ -57,26 +65,36 @@ export const ReactiveAggregate = (sub, collection, pipeline, options) => {
     }
   }
 
-  // track any changes on the collection used for the aggregation
-  const query = collection.find(options.observeSelector, options.observeOptions);
-  const handle = query.observeChanges({
-    added: debounce,
-    changed: debounce,
-    removed: debounce,
-    error(err) {
-      throw err;
-    }
+  if (localOptions.autoObserver) {
+    const query = collection.find(localOptions.observeSelector, localOptions.observeOptions);
+    localOptions.observers.push(query);
+  }
+
+  const handles = [];
+  // track any changes on the observed cursors
+  localOptions.obervers.forEach((query) => {
+    handles.push(query.observeChanges({
+      added: debounce,
+      changed: debounce,
+      removed: debounce,
+      error(err) {
+        throw err;
+      }
+    }));
   });
-  // observeChanges() will immediately fire an "added" event for each document in the query
-  // these are skipped using the initializing flag
+  // End of the setup phase.
+  
+  // Clear the initializing flag. From here, we're on autopilot
   initializing = false;
   // send an initial result set to the client
   Promise.await(update());
   // mark the subscription as ready
   sub.ready();
 
-  // stop observing the cursor when the client unsubscribes
+  // stop observing the cursors when the client unsubscribes
   sub.onStop(function () {
-    handle.stop();
+    handles.forEach(handle => {
+      stop();
+    });
   });
 };
