67e1f497a347dd88df32627f2bc867fd1029ee11
Rob Fallows <rob.fallows@gmail.com>
2023-12-04 14:15:45 +0000

    set versions from 3.0

--- package.js
+++ package.js
@@ -1,15 +1,14 @@
 Package.describe({
   name: 'tunguska:reactive-aggregate',
-  version: '1.3.11',
+  version: '2.0.0',
   summary: 'Publish aggregations reactively',
   git: 'https://github.com/robfallows/tunguska-reactive-aggregate',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.5');
+  api.versionsFrom('3.0');
   api.use('mongo');
   api.use('ecmascript');
-  api.use('promise');
   api.mainModule('aggregate.js', 'server');
 });
