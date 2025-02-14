74b6eab063b7ae5bd2426eca3982f16da11b8287
Tyler Thompson <tylerthompson@mac.lan>
2025-02-14 14:51:13 -0600

    removing lines 214-218: MongoInternals.NpmModule.ObjectID being undefined

--- aggregate.js
+++ aggregate.js
@@ -211,11 +211,6 @@ export const ReactiveAggregate = async (sub, collection = null, pipeline = [], o
           throw new TunguskaReactiveAggregateError('every aggregation document must have an _id');
         } else if (doc._id instanceof Mongo.ObjectID) {
           doc_id = doc._id.toHexString();
-        } else if (doc._id instanceof MongoInternals.NpmModule.ObjectID) {
-          // This means it was an ObjectID that got converted by rawCollection() methods
-          // that use the underlying MongoDB driver, so convert it back.
-          doc_id = doc._id.toString();
-          doc._id = new Mongo.ObjectID(doc_id);
         } else if (typeof doc._id === 'object') {
           // This is some other kind of object, so leave it as is.
           doc_id = doc._id.toString();
